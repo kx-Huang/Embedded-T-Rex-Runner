a480b9f7b6e271c3cd0cc5b85851599f24dc5ce3
kx-huang <Huang.KX@sjtu.edu.cn>
2022-04-29 23:44:51 -0500

    add: main files

---
+++ main.h
@@ -0,0 +1,17 @@
+/*
+ * main.h
+ *
+ *  Created on: Apr 27, 2022
+ *      Author: Kexuan Huang
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <task_button.h>
+#include <task_joystick.h>
+#include <task_lcd.h>
+#include <task_game.h>
+#include <ADC.h>
+
+#endif /* MAIN_H_ */
