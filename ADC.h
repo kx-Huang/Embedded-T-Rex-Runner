5b9cf40b30c167f57c3ae22b103bc97260747b97
kx-huang <Huang.KX@sjtu.edu.cn>
2022-04-29 23:46:21 -0500

    feat: ADC and random from hardware noise

---
+++ ADC.h
@@ -0,0 +1,15 @@
+/*
+ * ADC.h
+ *
+ *  Created on: Apr 29, 2022
+ *      Author: Kexuan Huang
+ */
+
+#ifndef ADC_H_
+#define ADC_H_
+
+#include <stdbool.h>
+
+void ADC14_IRQHandler(void);
+
+#endif /* ADC_H_ */
